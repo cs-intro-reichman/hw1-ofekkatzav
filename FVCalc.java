f8c16d6cf113e82c9d986c0f6427171df5017656
Ofek Katzav <ofekkatzav@h-MacBook-Pro-sl-Ofek.local>
2024-11-12 14:33:33 +0200

    fixed done 2

--- FVCalc.java
+++ FVCalc.java
@@ -5,8 +5,8 @@ public class FVCalc {
         double rate = Double.parseDouble(args[1]);
         int years = Integer.parseInt(args[2]);
         double futureVal = currentVal * Math.pow(1+(rate/100),years);
-        double Mispar = (int)Math.pow(2,4);
-        System.out.println("After "+years+" years, $" +currentVal+" saved at "+rate+"% will yield $"+futureVal);
-        System.out.println(Mispar);
+        double Mispar = Math.pow(2,4);
+        System.out.println("After "+years+" years, a $" +currentVal+" saved at "+rate+"% will yield $"+futureVal);
+        System.out.println((int)Mispar);
 	}
 }
